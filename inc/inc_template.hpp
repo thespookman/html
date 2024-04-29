b0a9dedd3910b715953927948f8e9f47431c376f
Sam Knoop <samknoop@gmail.com>
2024-04-29 08:23:18 +0100

    remove parameter include from inc_template

--- inc/inc_template.hpp
+++ inc/inc_template.hpp
@@ -1,11 +1,17 @@
 #pragma once
-#include "parameter.hpp"
-
 #include <list>
 #include <string>
 
 namespace spk {
 namespace html {
+class Parameter {
+  public:
+    std::string key;
+    std::string value;
+    bool        has_value;
+    Parameter (std::string key);
+    Parameter (std::string key, std::string value);
+};
 #define HTML_BIN(tag)                                                     \
     std::string tag (const std::string&          content = "",            \
                      const std::list<Parameter>& params  = {});            \
